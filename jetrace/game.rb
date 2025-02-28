df88f47c699083adf18c8684a1b8ee9f81dbc540
xordog <xordog@gmail.com>
2025-02-28 13:13:28 +0900

    Add template

---
+++ jetrace/game.rb
@@ -0,0 +1,17 @@
+class Game
+  def draw()
+    background 0
+  end
+
+  def key_down(code)
+  end
+
+  def key_up(code)
+  end
+end
+
+
+setup        {$game = Game.new}
+draw         {$game&.draw}
+key_pressed  {$game&.key_down key_code unless key_is_repeated}
+key_released {$game&.key_up   key_code}
