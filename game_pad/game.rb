e68bd84d0bfd8fdc4b2e86ef18717e83f067c32b
kaiba <kaibadash@gmail.com>
2025-03-14 21:23:57 +0900

    WIP: show all pressed key

---
+++ game_pad/game.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# ゲームを実装したクラス
+class Game
+  def initialize
+    @key_code = 'Press any key'
+  end
+
+  def draw
+    background(0)
+    scale(2, 2)
+
+    fill(255, 255, 255)
+    text_size(32)
+    text_align(CENTER, CENTER)
+    text(@key_code.to_s, 0, 0, width / 2, height / 2)
+  end
+
+  def key_down(key_code)
+    @key_code = key_code
+  end
+end
+
+setup do
+  $game = Game.new
+end
+
+draw do
+  $game&.draw
+end
+
+key_pressed do
+  $game&.key_down key_code
+end
